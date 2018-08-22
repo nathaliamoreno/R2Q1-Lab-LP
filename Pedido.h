d31f4ee29b600d75796ea0626ed2faa6084c9a04
nathaliamoreno <41310257+nathaliamoreno@users.noreply.github.com>
2018-08-22 15:57:00 -0300

    Update Pedido.h

--- Pedido.h
+++ Pedido.h
@@ -1,20 +1,25 @@
-#include "RestauranteCaseiro.h"
-#include "Pedido.h"
-#include "MesaDeRestaurante.h"
+#ifndef PEDIDO_H
+#define PEDIDO_H
+#include <string>
 
+class Pedido{
+    public:
+        Pedido();
+        Pedido(int n, std::string d, int q, double p);
+        void setNumero(int n);
+        void setDescricao(std::string d);
+        void setQuantidade(int q);
+        void setPreco(double p);
+        int getNumero();
+        std::string getDescricao();
+        int getQuantidade();
+        double getPreco();
+    private:
+        int numero;
+        std::string descricao;
+        int quantidade;
+        double preco;
+};
 
-RestauranteCaseiro::RestauranteCaseiro(){
+#endif // PEDIDO_H
 
-}
-int RestauranteCaseiro::adicionaAoPedido(Pedido p, int NovaQt){
-    this->pedido = p; //recebendo as informacoes do pedido da main que quer modificar
-    pedido.setQuantidade(NovaQt);
-    return pedido.getQuantidade(); //retornando o novo valor
-}
-double RestauranteCaseiro::calculaTotalRestaurante(double valor){ // valor do parametro com valor = ao MesaDeRestaurante
-    double totalRest =0; //total restaurante
-
-    totalRest += valor; //pega o valor da classe MesaDeRestaurante e soma ao total.
-
-    return totalRest;
-}
