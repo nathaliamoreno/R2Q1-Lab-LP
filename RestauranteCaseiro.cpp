e317256d3489f54ce36cb906b026e5f678c56d54
nathaliamoreno <41310257+nathaliamoreno@users.noreply.github.com>
2018-08-27 15:23:08 -0300

    Update RestauranteCaseiro.cpp

--- RestauranteCaseiro.cpp
+++ RestauranteCaseiro.cpp
@@ -8,12 +8,19 @@ RestauranteCaseiro::RestauranteCaseiro(){
 void RestauranteCaseiro::adicionaAoPedido(Pedido p, int m){
     mesa[m].adicionaAoPedido(p);
 }
+double RestauranteCaseiro::calculaTotalMesa(int m){
+    return mesa[m].calculaTotal();
+
+}
 double RestauranteCaseiro::calculaTotalRestaurante(){
-   static double totalRest =0; //total restaurante
+   double totalRest = 0; //total restaurante
 
      for(int i = 0; i < 10; i++){
         totalRest += mesa[i].calculaTotal() ; //pega o valor da classe MesaDeRestaurante e soma ao total.
      }
 
     return totalRest;
+}
+void RestauranteCaseiro::zeraPedidosMesa(int m){
+    mesa[m].zeraPedidos();
 }
